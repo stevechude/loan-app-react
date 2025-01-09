70d5fd95e2cd671408241f2c23c61c3b973d5c75
stevechude <stevechude@gmail.com>
2025-01-09 17:18:28 +0100

    test configuration bugs fixed

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,8 +1,13 @@
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
 describe("Renders App", () => {
   it("Renders App", () => {
-    render(<App />);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
   });
 });
